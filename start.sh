a6fb6ce828f1a93265e37a0e61855b64bcf1b83a
marko <markokanadateam2@]mail.com>
2025-05-26 19:32:05 +0200

    fix start

---
+++ start.sh
@@ -0,0 +1,7 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 20
+nvm use 20
+cd /media/Quizer/quiz/
+npm run dev
